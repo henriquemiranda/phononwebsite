bd6b74873e67d1584a4e77041ce0d6b15bb42685
Henrique Miranda <miranda.henrique@gmail.com>
2018-01-12 15:17:07 +0100

    Changed getCombinations from utils

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 const pi = 3.14159265359;
 
-function getCombination(selements) {
+function getCombinations(elements) {
     /* 
     Get combintations 2 by two based on:
     http://stackoverflow.com/questions/29169011/javascript-arrays-finding-the-number-of-combinations-of-2-elements
