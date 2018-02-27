3ac465a7791535f71c91c6ea743bfa68f55548e3
Henrique Miranda <miranda.henrique@gmail.com>
2018-02-27 11:30:11 +0100

    Fixed last problems in phononyaml detected by the mocha test suite.

--- src/phononyaml.js
+++ src/phononyaml.js
@@ -1,3 +1,7 @@
+import * as atomic_data from './atomic_data.js';
+import * as utils from './utils.js';
+import * as mat from './mat.js';
+
 var thz2ev = 33.35641;
 
 export class PhononYaml {
@@ -80,7 +84,7 @@ export class PhononYaml {
         }
 
         //get the number of repetitions
-        let pmat = matrix_multiply(lat,tmat);
+        let pmat = mat.matrix_multiply(lat,tmat);
 
         //get the atoms inside the unit cell
         this.atom_types = [];
@@ -93,13 +97,13 @@ export class PhononYaml {
             let atom = pc_atoms[i];
             let symbol   = PhononYaml.getYaml(['symbol'],atom);
             let position = PhononYaml.getYaml(['position','coordinates'],atom);
-            this.atom_numbers.push(atomic_number[symbol]);
+            this.atom_numbers.push(atomic_data.atomic_number[symbol]);
             this.atom_types.push(symbol);
             this.atom_pos_red.push(position);
             this.atom_pos_car.push(utils.red_car(position,lat));
         }
 
-        this.formula = get_formula(this.atom_types);
+        this.formula = utils.get_formula(this.atom_types);
         this.name = this.formula;
 
         let check_high_sym_qpoint = function(phonon_qpoint,highsym_qpts) {
@@ -165,7 +169,7 @@ export class PhononYaml {
         let average_mass = 0;
         let sqrt_atom_masses = [];
         for (let i=0;i<this.natoms;i++) {
-            let mass = atomic_mass[this.atom_numbers[i]]
+            let mass = atomic_data.atomic_mass[this.atom_numbers[i]]
             average_mass += mass;
             sqrt_atom_masses.push(Math.sqrt(mass));
         }
@@ -194,6 +198,6 @@ export class PhononYaml {
 
         this.addatomphase = true;
         this.lat = lat;
-        this.repetitions = getReasonableRepetitions(this.natoms,lat);
+        this.repetitions = utils.getReasonableRepetitions(this.natoms,lat);
     }
 }
